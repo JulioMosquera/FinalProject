4ab1891729afa0918587ab57d508c3f16ad51317
JulioMosquera <90935714+JulioMosquera@users.noreply.github.com>
2021-10-25 15:34:08 -0400

    Update maze.c
    first iteration

---
+++ maze.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "ascii_Arts&Mazes.c"
+
+
+int main(void)
+{
+
+  printf("Hello World\n");
+  printf("%s", maze1[0]);
+  //Hello
+
+  return 0;
+}
