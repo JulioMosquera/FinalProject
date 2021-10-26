c6c688207aba25aa6d9251cdf86250c0ef2eaa43
BKimberlin99 <kimberlin.bk99@gmail.com>
2021-10-26 10:57:52 -0400

    Created PrintScoreboard function

--- maze.c
+++ maze.c
@@ -3,6 +3,7 @@
 
 #include "ascii_Arts&Mazes.c"
 
+void PrintScoreboard();
 
 int main(void)
 {
@@ -10,6 +11,24 @@ int main(void)
   printf("Hello World\n");
   printf("%s", maze1[0]);
   //Hello
+  
+  PrintScoreboard();
 
   return 0;
 }
+
+
+//Todo: update PrintScoreboard to show proper values
+void PrintScoreboard() {
+  printf(" ------------------------------------------------------\n");
+  printf("|                    SCOREBOARD                        |\n");
+  printf("|------------------------------------------------------|\n");
+  printf("| Score      |          |  Location X   |              |\n");
+  printf("|------------------------------------------------------|\n");
+  printf("| Multiplier |          |  Location Y   |              |\n");
+  printf("|------------------------------------------------------|\n");
+  printf("| Moves Left |          |                              |\n");
+  printf(" ------------------------------------------------------\n");
+}
+
+
