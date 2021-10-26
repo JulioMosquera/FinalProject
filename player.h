f25b8103223a6cd02ccb29d14217daf3dafa0497
JulioMosquera <90935714+JulioMosquera@users.noreply.github.com>
2021-10-26 10:51:33 -0400

    Added functions of player

--- player.h
+++ player.h
@@ -1,15 +1,15 @@
 #ifdef FINALPROJECT_PLAYER_STRUCT
 #define FINALPROJECT_PLAYER_STRUCT
 
-typedef struct Player
+typedef struct Player_Struct
 {
   int location[2];
   int movesLeft;
   int score;
   int scoreMultipler;
-}player;
+}Player;
 
-player initPlayer(int l[2], int ml, int s, int sm);
-player moverplayer(int l[2]);
-player updateScore(int s);
-player addMultipler(int sm);
+Player initPlayer(int l[2], int ml, int s, int sm);
+Player moverplayer(Player p, int l[2]);
+Player updateScore(Player p, int s);
+Player addMultipler(Player p);
