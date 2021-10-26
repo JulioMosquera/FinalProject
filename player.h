80ea827e202276633dd8013920690f9e17323391
JulioMosquera <90935714+JulioMosquera@users.noreply.github.com>
2021-10-26 10:22:22 -0400

    Update player.h
    added initial play struct functions

--- player.h
+++ player.h
@@ -3,5 +3,13 @@
 
 typedef struct Player
 {
+  int location[2];
+  int movesLeft;
+  int score;
+  int scoreMultipler;
+}player;
 
-}
+player initPlayer(int l[2], int ml, int s, int sm);
+player moverplayer(int l[2]);
+player updateScore(int s);
+player addMultipler(int sm);
