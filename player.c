ce6ffaf5162204eb855743ace091eb0ab70bf054
Randy Manraj <77643410+RandyManraj@users.noreply.github.com>
2021-10-29 15:16:54 +0000

    edited comments

--- player.c
+++ player.c
@@ -1,60 +1,44 @@
 #include <stdio.h>
-
 #include "player.h"
-
-// Initializes player to the given location x = 1, y = 1;
-// Score / Multiplier and timespent set to default values;
 
 Player initPlayer(int loc[2], int ml, int s, int sm)
 {
     Player newPlayer;
-
-    newPlayer.location[0] = loc[0];
-    newPlayer.location[1] = loc[1];
-    newPlayer.movesLeft = ml;
-    newPlayer.score = 0;
+    newPlayer.location[0] = loc[0]; // Initializes player to the given location x = 1
+    newPlayer.location[1] = loc[1]; // y = 1
+    newPlayer.movesLeft = ml; // Moves left set to 200
+    newPlayer.score = 0; //Score set to 0 along with the rest given default values
     newPlayer.scoreMultipler = 1;
     newPlayer.timeSpent = 0;
     return newPlayer;
 }
 
-// moves player to new location
-
-Player moverplayer(Player p, int loc[2])
+Player moverplayer(Player p, int loc[2]) // moves player to new location
 {
-  p.location[0] = loc[0];
-  p.location[1] = loc[1];
+  p.location[0] = loc[0];  // Player X set to new X value
+  p.location[1] = loc[1]; //  Player Yset to new Y value
   return p;
 }
-
-// Multiplies the new score recieved by the multipliers
-// obtained in game and adds it to the previous sum
 
 Player updateScore(Player p, int s)
 {
-  int tempscore = s * p.scoreMultipler;
-  p.score = p.score + tempscore;
+  int tempscore = s * p.scoreMultipler; // Multiplies the new score recieved by the multipliers
+  p.score = p.score + tempscore;        // obtained in game and adds it to the previous sum
   return p;
 }
-
-// Increases the score multiplier by 1
 
 Player addMultipler(Player p)
 {
-  p.scoreMultipler += 1;
+  p.scoreMultipler += 1; // Increases the score multiplier by 1
   return p;
 }
 
-// When the remaining moves becomes less than 0
-// Displays the game over screen
-// Resets player location and moves and sets it up for the next map
-
-int gameOver(Player p)
+int gameOver(Player p)  
 {
-  if(p.movesLeft < 0)
+  if(p.movesLeft < 0)   // When the remaining moves becomes less than 0
   {
     int restart[2] = {1,1};
-    initPlayer(restart ,200, 0, 1);
+    initPlayer(restart ,200, 0, 1);  // Resets player location and moves and sets it up for the next map
     return 0;
   }
   else
@@ -62,42 +46,29 @@ int gameOver(Player p)
     return 1;
   }
 }
-
-// When the player's location reaches the X which is located at the end of the map
-// if you reach the end, return false
-// else return true
 
 int youWon(Player p)
 {
   Player newPlayer = p;
-  if(newPlayer.location[1] == 25 && newPlayer.location[0] == 49)
+  if(newPlayer.location[1] == 25 && newPlayer.location[0] == 49) // When the player's location reaches the end of the map
   {
-    return 0;
+    return 0;  //return false
   }
   else
   {
-    return 1;
+    return 1; //return true
   }
   
 }
-
-// If the input is in the positive X direction
-// // Checks what the next character on the board is
-// If the next space the player is moving to is 
-// # * ! $ + -
-// Do the following to influence the game
-
-// True was used to progress X or Y in the positive direction
-// Whereas false was used to progress in the negative direction
 
 Player CheckNextCharX(char curMaze[27][52], char origMaze[27][52], int i, Player p)
 {
   Player newPlayer = p;
   int x = newPlayer.location[0];
   int y = newPlayer.location[1];
-  if( i == 1)
+  if( i == 1)  // If the input is in the positive X direction (True)
   {
-    if(curMaze[y][x + 1] != '#')
+    if(curMaze[y][x + 1] != '#') // // Checks what the next character on the board is, in this case a wall
     {
       curMaze[y][x] = ' ';
       x++;
@@ -110,7 +81,7 @@ Player CheckNextCharX(char curMaze[27][52], char origMaze[27][52], int i, Player
       }
       else if(origMaze[y][x] == '*')
       {
-        newPlayer = updateScore(newPlayer, 10);
+        newPlayer = updateScore(newPlayer, 10); // Do the following to influence the game
       }
       else if(origMaze[y][x] == '!')
       {
@@ -130,9 +101,8 @@ Player CheckNextCharX(char curMaze[27][52], char origMaze[27][52], int i, Player
       }
     }
   }
-  // If the input is in the negative X direction
-  // Same as above is applied
-  if( i == 0)
+
+  if( i == 0)  // If the input is in the negative X direction (False)
   {
     if(curMaze[y][x - 1] != '#')
     {
@@ -174,8 +144,7 @@ Player CheckNextCharY(char curMaze[27][52], char origMaze[27][52], int i, Player
   Player newPlayer = p;
   int x = newPlayer.location[0];
   int y = newPlayer.location[1];
-  // If input is in the positive Y direction
-  if( i == 1)
+  if( i == 1)   // If input is in the positive Y direction
   {
     if(curMaze[y + 1][x] != '#')
     {
@@ -210,8 +179,7 @@ Player CheckNextCharY(char curMaze[27][52], char origMaze[27][52], int i, Player
       }
     }
   }
-  // If input is in the negative Y direction
-  if( i == 0)
+  if( i == 0)   // If input is in the negative Y direction
   {
     if(curMaze[y - 1][x] != '#')
     {
